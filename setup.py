aef548e647fb2ce9ca59cf8d6c6e5e7301562d71
Roman <megaclaff@gmail.com>
2017-05-23 22:54:37 +0300

    font-awsome fonts are not loading // fixed - added fonts to font-awsome/fonts and updated MANIFEST.in - bumped to version 0.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("djeddit", "__init__.py")
+version = '0.1.3'
 
 
 if sys.argv[-1] == 'publish':
