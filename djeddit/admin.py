ea4d2ca16c44b221dbe69ac66fafc851adb48c7b
Logmytech <Logmytech@users.noreply.github.com>
2017-12-12 18:26:29 +0530

    Added admin registrations as test

--- djeddit/admin.py
+++ djeddit/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from models import Topic
+from models import Post
+from models import Thread
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Thread)
+admin.site.register(Topic)
