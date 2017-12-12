8bf36a54b472dc22ba26452f38990f0357361490
Logmytech <Logmytech@users.noreply.github.com>
2017-12-12 17:44:49 +0530

    Adde IP address info

--- djeddit/views.py
+++ djeddit/views.py
@@ -1,16 +1,22 @@
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render, redirect
-from django.http import Http404, HttpResponseForbidden, HttpResponseBadRequest, HttpResponseRedirect
+#Python standard imports
+import json
+import logging
 
+#Core Dajngo imports
+from django.http import JsonResponse, HttpResponse, Http404, HttpResponseForbidden, HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
+
+#Third-party app imports
+from ipware.ip import get_ip
+
+#Imports from our apps
 from djeddit.forms import TopicForm, ThreadForm, PostForm
 from djeddit.models import Topic, Thread, Post, UserPostVote
 from djeddit.templatetags.djeddit_tags import postScore
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import user_passes_test
-import json
 
-import logging
 
 
 # Create your views here.
@@ -27,7 +33,17 @@ def createThread(request, topic_title=None):
                     post = postForm.save(commit=False)
                     thread.op = post
                     thread.topic = topic
+
+                    ip = get_ip(request)
+                    if ip is not None:
+                        post.ip_address = ip
+                    
+                    ua = request.META.get('HTTP_USER_AGENT', '')
+                    if ua:
+                        post.user_agent = ua 
+                    
                     thread.save()
+                    
                     if request.user.is_authenticated():
                         post.created_by = request.user
                     post.save()
@@ -131,6 +147,15 @@ def replyPost(request, post_uid=''):
         if postForm.is_valid():
             post = postForm.save(commit=False)
             post.parent = repliedPost
+                                
+            ip = get_ip(request)
+                if ip is not None:
+                    post.ip_address = ip
+                  
+            ua = request.META.get('HTTP_USER_AGENT', '')
+            if ua:
+                post.user_agent = ua 
+            
             if request.user.is_authenticated():
                 post.created_by = request.user
             post.save()
