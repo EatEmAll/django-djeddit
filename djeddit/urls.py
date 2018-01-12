1f3dc5aa850d4f53632f8e7a034a8cdbc74a2060
Sravan Kumar <Logmytech@users.noreply.github.com>
2018-01-12 19:25:32 +0530

    Added things as optional for using reverse in JS

--- djeddit/urls.py
+++ djeddit/urls.py
@@ -24,11 +24,11 @@ topic_prefix = '%s/' % settings.TOPICS_URL if settings.TOPICS_URL else ''
 
 urlpatterns = [
     url(r'^%s$' % (topic_prefix + '/?' if topic_prefix else ''), views.topicsPage, name='topics'),
-    url(r'^lock_thread/(\d+)/?$', views.lockThread, name='lockThread'),
+    url(r'^lock_thread/(\d+)?/?$', views.lockThread, name='lockThread'),
     url(r'^reply_post/([\w\-]{36})?/?$', views.replyPost, name='replyPost'),
     url(r'^edit_post/([\w\-]{36})?/?$', views.editPost, name='editPost'),
     url(r'^vote_post/?$', views.votePost, name='votePost'),
-    url(r'^delete_post/([\w\-]{36})/?$', views.deletePost, name='deletePost'),
+    url(r'^delete_post/([\w\-]{36})?/?$', views.deletePost, name='deletePost'),
     url(r'^load_additional_replies/?$', views.loadAdditionalReplies, name='loadAdditionalReplies'),
     url(r'^user/(.+)/summary/?$', views.userSummary, name='userSummary'),
     url(r'^user/(.+)/threads/?$', views.userThreadsPage, name='userThreads'),
@@ -36,7 +36,7 @@ urlpatterns = [
     url(r'^users/?$', views.usersPage, name='usersPage'),
     url(r'^set_user_status/?$', views.setUserStatus, name='setUserStatus'),
     url(r'^%s([a-zA-Z0-9\-_]+)/?$' % topic_prefix, views.topicPage, name='topicPage'),
-    url(r'^%s([a-zA-Z0-9\-_]+)/delete_topic/?$' % topic_prefix, views.deleteTopic, name='deleteTopic'),
+    url(r'^%s([a-zA-Z0-9\-_]+)?/delete_topic/?$' % topic_prefix, views.deleteTopic, name='deleteTopic'),
     url(r'^%s([a-zA-Z0-9\-_]+)/newthread/?$' % topic_prefix, views.createThread, name='createThread'),
     url(r'^%s(?P<topic_title>[a-zA-Z0-9\-_]+)/(?P<thread_id>\d+)/(?P<slug>[a-z0-9\-]+)?/?$' % topic_prefix, views.threadPage, name='threadPage'),
 ]
